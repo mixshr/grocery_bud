75f5602a15e40ac54e554c115c64fb18bfc72abe
mihailsarenko <mixail.sharenko@gmail.com>
2024-06-03 23:24:23 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,37 @@ import Form from "./Form";
 import {nanoid} from "nanoid";
 import Items from "./Items";
 
+const setLocaleStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    list = JSON.parse(list);
+  } else {
+    list = [];
+  }
+};
+const defaultList = JSON.parse(localStorage.getItem('item') || '[]');
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
+  getLocalStorage();
   const addItem = (itemName) => {
     const newItem = {
       name:itemName,
       completed: false,
       id:nanoid(),
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocaleStorage(newItems);
   };
 
   const removeItem = (itemID) => {
     const newItems = items.filter((item) => itemID !== item.id);
     setItems(newItems);
+    setLocaleStorage(newItems);
   }
 
   return <section className='section-center'>
@@ -26,4 +42,4 @@ function App() {
   </section>
 }
 
-export default App
+export default App;
